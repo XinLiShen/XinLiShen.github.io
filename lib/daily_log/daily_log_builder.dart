d1db465f725a881e283d1aae490299d2e8e6e7a6
ashenn_0 <ashenn_0@outlook.com>
2024-09-14 13:46:10 +0800

    new featrue daliy log init

---
+++ lib/daily_log/daily_log_builder.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DailyLogBuilder extends StatefulWidget {
+  const DailyLogBuilder({ Key? key }) : super(key: key);
+
+  @override
+  _DailyLogBuilderState createState() => _DailyLogBuilderState();
+}
+
+class _DailyLogBuilderState extends State<DailyLogBuilder> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
